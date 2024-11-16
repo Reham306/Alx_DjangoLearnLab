8b86b1bf9c4e5f14eb02dcabd21da8e47c9ee00e
Reham Habib <rehamaly1997@gmailcom>
2024-11-16 11:45:57 -0800

    edited

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Book
 
-
 # Register the Book model
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Query all books
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
