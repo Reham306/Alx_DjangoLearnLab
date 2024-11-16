dbce89fd2ce3df36800ad7493094d3f3baaa63c5
Reham Habib <rehamaly1997@gmailcom>
2024-11-16 12:45:43 -0800

    edited

--- django-models/urls.py
+++ django-models/urls.py
@@ -6,3 +6,12 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+]
